74f4c9d94da5d6a81429e80a52292012026b92e0
Undead34 <maizogabriel@gmail.com>
2022-06-17 18:16:17 -0400

    Commit

---
+++ app/modules/fileSystem.js
@@ -0,0 +1,113 @@
+const fs = require("fs");
+
+/**
+ * Remove recursively a folder
+ * @param {*} path path to remove folder
+ * @returns null
+ */
+ const deleteFolder = (path) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      fs.rmdirSync(path, { recursive: true });
+      resolve();
+    } catch (error) {
+      console.log(error.message);
+      rejects();
+    }
+  });
+}
+
+/**
+ * Create a folder
+ * @param {string} path path to create
+ * @returns null
+ */
+ const createFolder = (path) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      fs.mkdirSync(path, { recursive: true });
+      resolve();
+    } catch (error) {
+      console.log(error.message);
+      rejects();
+    }
+  });
+}
+
+/**
+ * Validate if a file exists
+ * @param {string} path path to validate
+ * @returns boolean
+ */
+ const exists = (path) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      let exists = fs.existsSync(path);
+      resolve(exists);
+    } catch (error) {
+      console.log(error.message);
+      rejects();
+    }
+  });
+}
+
+/**
+ * Read data from file
+ * @param {string} path path file to read
+ * @returns data readed
+ */
+ const readFile = (path) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      let data = fs.readFileSync(path);
+      resolve(data);
+    } catch (error) {
+      console.log(error.message);
+      rejects();
+    }
+  });
+}
+
+/**
+ * List files in a folder
+ * @param {string} path path to list directory
+ * @returns array of files
+ */
+ const listFiles = (path) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      let files = fs.readdirSync(path);
+      resolve(files);
+    } catch (error) {
+      console.log(error.message);
+      rejects();
+    }
+  });
+}
+
+/**
+ * Remove a file
+ * @param {*} path path to remove file
+ * @returns null
+ */
+const deleteFile = (path) => {
+  return new Promise((resolve, rejects) => {
+    try {
+      fs.unlinkSync(path);
+      resolve();
+    } catch (error) {
+      console.log(error.message);
+      rejects();
+    }
+  });
+}
+
+
+module.exports = {
+  listFiles,
+  readFile,
+  deleteFile,
+  deleteFolder,
+  createFolder,
+  exists
+}
--- public/electron.js
+++ public/electron.js
@@ -1,39 +1,72 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, ipcMain } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const fs = require("fs");
 
-const path = require('path');
-const isDev = require('electron-is-dev');
+// IPC main events listeners
+const listenEvents = async () => {
+  let eventPath = path.join(__dirname, "../app/events");
+
+  const eventFiles = (await fs.readdirSync(eventPath)).filter((file) =>
+    file.endsWith(".js")
+  );
+
+  for (const file of eventFiles) {
+    const event = require(`../app/events/${file}`);
+    if (event.once) {
+      if (event.handle)
+        ipcMain.handleOnce(event.name, (...args) => event.execute(...args));
+      else ipcMain.once(event.name, (...args) => event.execute(...args));
+    } else {
+      if (event.handle)
+        ipcMain.handle(event.name, (...args) => event.execute(...args));
+      else ipcMain.on(event.name, (...args) => event.execute(...args));
+    }
+    delete require.cache[require.resolve(`../app/events/${file}`)]; // delete cache
+  }
+
+  return true;
+};
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 900, 
-    height: 680, 
+    width: 900,
+    height: 680,
     webPreferences: {
-      nodeIntegration: true
-    }
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        enableRemoteModule: false,
+        preload: path.join(__dirname, "preload.js"),
+      },
+    },
   });
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
     mainWindow.webContents.openDevTools();
   }
-  mainWindow.on('closed', () => mainWindow = null);
+  listenEvents();
+  mainWindow.on("closed", () => (mainWindow = null));
   mainWindow.removeMenu();
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
