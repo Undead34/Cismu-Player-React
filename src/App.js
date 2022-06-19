91ae3653e95c6a050cd3ba5f4fbecf8930bc1a43
Undead34 <maizogabriel@gmail.com>
2022-06-19 15:40:07 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
 import ProgressBar from "./components/progress-bar.component";
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  window.api.receive("progress-bar", (progress)=>{
-    console.log(progress)
-    setCount(progress);
-  })
-
   return (
     <div className="App">
-      <ProgressBar key="0" bgcolor="#6a1b9a" completed={count} />
+      <ProgressBar key="0" bgcolor="#6a1b9a" completed="10" />
     </div>
   );
 }
--- src/components/progress-bar.component.js
+++ src/components/progress-bar.component.js
@@ -1,44 +1,56 @@
 import React from "react";
 
-const ProgressBar = (props) => {
-  const { bgcolor, completed } = props;
 
-  const containerStyles = {
-    height: 20,
-    width: '100%',
-    backgroundColor: "#e0e0de",
-    borderRadius: 50,
-    margin: 0
+const containerStyles = {
+  height: 20,
+  width: '100%',
+  backgroundColor: "#e0e0de",
+  borderRadius: 50,
+  margin: 0
+}
+
+const fillerStyles = {
+  height: '100%',
+  width: `0%`,
+  backgroundColor: '#fff',
+  borderRadius: 'inherit',
+  textAlign: 'right',
+  transition: 'width 1s ease-in-out',
+}
+
+const labelStyles = {
+  padding: 5,
+  color: 'white',
+  fontWeight: 'bold'
+}
+
+class ProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: 0,
+      bgcolor: props.bgcolor,
+      textcolor: props.textcolor,
+    };
   }
 
-  const fillerStyles = {
-    height: '100%',
-    width: `${completed}%`,
-    backgroundColor: bgcolor,
-    borderRadius: 'inherit',
-    textAlign: 'right',
-    transition: 'width 1s ease-in-out',
-  }
-
-  const labelStyles = {
-    padding: 5,
-    color: 'white',
-    fontWeight: 'bold'
-  }
-
-  return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}>
-        <span style={labelStyles}
-          role="progressbar"
-          aria-valuenow={completed}
-          aria-valuemin="0"
-          aria-valuemax="100">
-            {`${completed}%`}
-        </span>
+  render() {
+    return (
+      <div style={containerStyles}>
+        <div style={fillerStyles}>
+          <span
+            style={labelStyles}
+            role="progressbar"
+            aria-valuenow={0}
+            aria-valuemin="0"
+            aria-valuemax="100"
+          >
+            {`${0}%`}
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProgressBar;
